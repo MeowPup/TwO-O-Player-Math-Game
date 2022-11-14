8a36f4bda525e8e0cd818d5a6ff971558d31e71b
EVillett <emily.villett@gmail.com>
2022-11-14 13:55:50 -0800

    updated game class

--- game.rb
+++ game.rb
@@ -2,18 +2,55 @@ require './players'
 require './questions'
 
 class Game
-  
-  # create the players
-  def initialize(name)
-    @name = name
-    @player1 = Player.new("Player 1")
-    @player2 = Player.new("Player 2")
+
+  def initialize()
+    puts "========================================"
+    puts "| Welcome to the two player math game! |"
+    puts "========================================"
+    @gameEnd = false;
+    player1 = Players.new("P1")
+    player2 = Players.new("P2")
+    current = player1
+    question = Questions.new
+
+    # ask questions, check answer
+    while (!@gameEnd)
+      question.ask_question
+      question.check_answer
+      
+      # Checks if the answer is correct and updates lives
+      if !question.correct
+        current.lose_life
+        puts "Seriously? No!" 
+        puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+      else 
+        puts "YES! You are correct."  
+      end
+
+      # Ends the game when player hits 0 lives
+      if current.lives == 0
+        puts "============ GAME OVER ==============="
+        puts "|      #{current.name} has lost the game!         |"
+        puts "=========== GOOD BYE! ================"
+        gameEnd
+      end    
+      
+
+      # switching between players for questions
+      question = Questions.new
+      if current == player1
+        current = player2
+      else 
+        current = player1
+      end
+
+    end
   end
 
-
-    
-
-
+  def gameEnd
+    @gameEnd = true;
+  end
 
 end
+
 
--- questions.rb
+++ questions.rb
@@ -1,20 +1,25 @@
 class Questions
 
+  attr_reader :number1, :number2, :answer, :userInput, :correct;
+
   # initialize question and answer
   def initialize()
     @number1 = rand(1...20)
     @number2 = rand(1...20)
     @answer = @number1 + @number2
+    correct = false;
   end
 
   # ask the question
   def ask_question
-    puts "#{name}: What does #{number1} plus #{number2} equal?"
+    puts "What does #{number1} plus #{number2} equal?"
     @userInput = gets.chomp.to_i
   end
 
   # check the answer
-  def check_answer(playerAnswer)
-    @answer == playerAnswer
+  def check_answer
+    if @answer == @userInput
+      @correct = true;
+    end
   end
 end
